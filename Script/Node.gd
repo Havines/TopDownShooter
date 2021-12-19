34c60439bd9cecc01acee4571682b27935ad3021
Havines <55022144+Havines@users.noreply.github.com>
2021-12-19 20:11:06 +0500

    Some little fix

--- Script/Node.gd
+++ Script/Node.gd
@@ -1,19 +1,7 @@
-extends Node
+extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://Scens/Stage.tscn")
+	var ch = get_tree().change_scene("res://Scens/Stage.tscn")
+	assert(ch == OK)
